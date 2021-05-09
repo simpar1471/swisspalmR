2357d066ae3af0dfb57c26c01e1354d9676e755e
Simon Parker <65285181+awadafuk@users.noreply.github.com>
2021-05-10 00:36:16 +0100

    Adding extras not in last commit

--- R/functions.R
+++ R/functions.R
@@ -1,21 +1,28 @@
 #' @name sendToSWISSpalm
 #' @title Send to SWISSpalm
 #' @description Send protein identifiers to SWISSpalm
-#' @param identifiers foo1
-#' @param output.type foo2
-#' @param output.dir foo3
+#' @param identifiers Vector of protein identifiers
+#' @param dataset.value Value indicating the desired dataset
+#' @param species.value Value indicating the species preference
+#' @param output.dir If specified, indicates the directory in which the results will be saved.
 #' @return Saves SWISSpalm output in specified user-specified output directory.
 #' @author Simon Parker  \email{tadeo5@hotmail.co.uk}
 #' @export
-sendToSWISSpalm <- function(identifiers, dataset.int = 1, species.int = 1,
-                            output.type = "text", output.dir){
+sendToSWISSpalm <- function(identifiers, dataset.value = 1, species.value = 1, output.dir){
   if(missing(identifiers)) stop("You must provide protein identifiers.")
-  if(missing(output.dir)) stop("You must provide an output directory.")
 
-  swisspalm_json <- rjson::toJSON(list(ids = identifiers,
-                                       dataset = dataset.int,
-                                       species = species.int,
-                                       output = output.type))
+#  checkmate::checkPathForOutput(x = tempfile(pattern = file.path(output.dir, "test_file"), fileext = ".tmp"))
   message("Sending identifiers to SWISSpalm.")
+  swisspalm_json <- rjson::toJSON(list(ids = identifiers,
+                                       dataset = dataset.value,
+                                       species = species.value,
+                                       output = output.type))
+
+  response <- httr::GET("https://www.swisspalm.org/", )
+
   message(paste0("Retrieving ", output.type, " from SWISSpalm."))
+  swisspalm_output <- rjson::fromJSON(response$content)
+
+  if(!missing(output.dir)){ write(swisspalm_output, file = file.path(output.dir, "swisspalm_output.txt")) }
+
 }
