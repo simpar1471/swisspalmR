3bdd674491de156568a526e70faa6d0f64e03175
Simon Parker <65285181+awadafuk@users.noreply.github.com>
2021-05-10 00:35:56 +0100

    Writing a bit more

--- R/functions.R
+++ R/functions.R
@@ -7,10 +7,15 @@
 #' @return Saves SWISSpalm output in specified user-specified output directory.
 #' @author Simon Parker  \email{tadeo5@hotmail.co.uk}
 #' @export
-sendToSWISSpalm <- function(identifiers, output.type = "vec", output.dir){
+sendToSWISSpalm <- function(identifiers, dataset.int = 1, species.int = 1,
+                            output.type = "text", output.dir){
   if(missing(identifiers)) stop("You must provide protein identifiers.")
   if(missing(output.dir)) stop("You must provide an output directory.")
 
+  swisspalm_json <- rjson::toJSON(list(ids = identifiers,
+                                       dataset = dataset.int,
+                                       species = species.int,
+                                       output = output.type))
   message("Sending identifiers to SWISSpalm.")
   message(paste0("Retrieving ", output.type, " from SWISSpalm."))
 }
