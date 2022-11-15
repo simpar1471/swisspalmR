be4dd42f6b396e3594d3cce0df5de6097a61048f
awadafuk <65285181+awadafuk@users.noreply.github.com>
2022-11-15 22:54:04 +0000

    Added docs for data files

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Mappings for dataset.value in getSWISSpalmData()
+#'
+#' @format ## `dataset_values`
+#' A vector of numbers with corresponding names which correspond key-value pairs of dataset.value
+"dataset_values"
+
+#' Mappings for species.value parameter in getSWISSpalmData()
+#'
+#' @format ## `dataset_values`
+#' A vector of numbers with corresponding names which correspond key-value pairs of species.value
+"species_values"
