0fd27c4418859e68a1daeec744cbae5e1778ffac
awadafuk <65285181+awadafuk@users.noreply.github.com>
2022-11-15 22:55:11 +0000

    Added preliminary testing suite

---
+++ tests/testthat/test-getSWISSpalmData.R
@@ -0,0 +1,8 @@
+# TODO: Implement testing suite! Test memoisation, different dataset/species values, etc.
+
+# test <- getSWISSpalmData(c("P09573", "P09553"), dataset.value = 1, species.value = 1)
+# test2 <- getSWISSpalmData(c("P09573", "P09553"), dataset.value = 1, species.value = 1)
+# test3 <- getSWISSpalmData(c("P05067", "O00161", "P04899"), dataset.value = 1, species.value = 1)
+# test_that("multiplication works", {
+#   expect_equal(2 * 2, 4)
+# })
