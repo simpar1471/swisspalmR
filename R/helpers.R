c9685db3f2d20a998e910f1216a94dc1f41bfce3
Simon Parker <65285181+awadafuk@users.noreply.github.com>
2023-09-13 00:30:00 +0100

    Update swissPalm.R to download .txt data from SwissPalm proteins site; this increases the information available to end users. Also param/docs changes.

--- R/helpers.R
+++ R/helpers.R
@@ -29,6 +29,31 @@ is_invalid_param <- function(param, valid_values, default) {
   } else FALSE
 }
 
+#' Construct an HTTP GET request for SwissPalm
+#' @param user User agent for the HTTP request.
+#' @keywords internal
+swisspalm_GET_req <- function(user) {
+  "https://swisspalm.org/proteins/refresh_form?first_load=true" |>
+    httr2::request() |>
+    httr2::req_user_agent(string = user) |>
+    httr2::req_retry(max_tries = 5,
+                     is_transient = ~ httr2::resp_status(.x) %in% 500)
+}
+
+#' Construct an HTTP POST request for SwissPalm
+#' @param headers A named list of headers and their values for the POST request.
+#' @param xhr A list of XHR components and their values for the POST request.
+#' @param user User agent for the HTTP request.
+#' @keywords internal
+swisspalm_POST_req <- function(headers, xhr, user) {
+  httr2::request(base_url = "https://swisspalm.org/proteins/search") |>
+    httr2::req_headers(!!!headers) |>
+    httr2::req_user_agent(string = user) |>
+    httr2::req_body_multipart(!!!xhr) |>
+    httr2::req_retry(max_tries = 5,
+                     is_transient = ~ httr2::resp_status(.x) %in% 500)
+}
+
 #' @keywords internal
 swissPalm_cookie <- function(resp) {
   cookie <- resp |>
@@ -48,6 +73,11 @@ csrf_token <- function(resp_html, form_id) {
 }
 
 #' @keywords internal
+found_as_factor <- function(x) {
+  factor(x, levels = c("found", "not found in database", "not found at all"))
+}
+
+#' @keywords internal
 extract_not_found <- function(POST_html) {
   # Get which proteins weren't found in the search
   POST_span_elems <- rvest::html_elements(POST_html, css = "span")
@@ -56,9 +86,9 @@ extract_not_found <- function(POST_html) {
   str_not_found_in_db <- "list_not_found"
   str_not_found_at_all <- "list_not_found_at_all"
 
-  purrr::map_dfr(
-    .x = list(str_not_found_in_db, str_not_found_at_all),
-    .f = \(x) {
+  lapply(
+    X = list(str_not_found_in_db, str_not_found_at_all),
+    FUN = \(x) {
       proteins <- POST_span_elems[which(POST_span_elems_id == x)] |>
         strsplit(split = ", ") |>
         unlist() |>
@@ -67,9 +97,62 @@ extract_not_found <- function(POST_html) {
                       yes = "not found in database",
                       no = "not found at all")
       if (!length(proteins) == 0) {
-        data.frame(`Query identifier` = proteins,
-                   `Found` = found)
-      } else invisible()
+        out <- data.frame(Query_identifier = proteins,
+                          Found_in_SwissPalm = found_as_factor(found))
+        out[, swissPalm_table_colnames()[-1]] <- NA
+        out_ordered <- c(swissPalm_table_colnames(), "Found_in_SwissPalm")
+        out[, out_ordered]
+      } else {
+        NULL
+      }
     }
+  ) |>
+    do.call(what = "rbind")
+}
+
+#' @keywords internal
+swissPalm_table_colnames <- function() {
+  invisible(
+     c("Query_identifier", "UniProt_AC", "UniProt_ID", "UniProt_status",
+       "Organism", "Gene_names","Description",
+       "Number_of_palmitoyl_proteomics_articles",
+       "Number_of_palmitoyl_proteomics_studies_where_the_protein_appears_in_a_high_confidence_hit_list",
+       "Number_of_technique_categories_used_in_palmitoyl_proteomics_studies",
+       "Technique_categories_used_in_palmitoyl_proteomics_studies",
+       "Number_of_targeted_studies", "Targeted_studies__PMIDs", "PATs", "APTs",
+       "Number_of_sites", "Sites_in_main_isoform", "Number_of_isoforms",
+       "Max_number_of_cysteines",
+       "Max_number_of_cysteines_in_TM_or_cytosolic_domain",
+       "Predicted_to_be_S_palmitoylated",
+       "Predicted_to_be_S_palmitoylated_in_cytosolic_domains",
+       "Protein_has_hits_in_SwissPalm",
+       "Orthologs_of_this_protein_have_hits_in_SwissPalm")
   )
+}
+
+#' Generate list-columns where necessary in SwissPalm tables
+#' @keywords internal
+convert_swissPalm_table_listcols <- function(sp_tbl) {
+  col2list <- function(col, as_numeric = FALSE) {
+    col <- lapply(X = as.character(col),
+                  FUN = \(x) {
+                    if (is.na(x) || x == "") {
+                      out <- NA
+                    } else {
+                      out <- unlist(strsplit(x, split = ", "))
+                    }
+                    if (as_numeric) {
+                      as.numeric(out)
+                    } else out
+                  })
+  }
+  sp_tbl$Gene_names <- col2list(sp_tbl$Gene_names)
+  sp_tbl$Targeted_studies__PMIDs <- col2list(sp_tbl$Targeted_studies__PMIDs)
+  sp_tbl$Technique_categories_used_in_palmitoyl_proteomics_studies <-
+    col2list(sp_tbl$Technique_categories_used_in_palmitoyl_proteomics_studies)
+  sp_tbl$PATs <- col2list(sp_tbl$PATs)
+  sp_tbl$APTs <- col2list(sp_tbl$APTs)
+  sp_tbl$Sites_in_main_isoform <- col2list(sp_tbl$Sites_in_main_isoform,
+                                           as_numeric = TRUE)
+  sp_tbl
 }
--- R/swissPalm.R
+++ R/swissPalm.R
@@ -1,37 +1,42 @@
 #' Retrieve palmitoylation data from the swissPalm database
 #'
-#' @param protein_id Character vector with protein identifiers. Should be an ID
+#' @param query_id Character vector with protein identifiers. Should be an ID
 #' type supported by SwissPalm (UniProt AC, UniProt secondary AC, UniProt ID,
 #' UniProt gene name, Ensembl protein, Ensembl gene, Refseq protein ID, IPI ID,
 #' UniGene ID, PomBase ID, MGI ID, RGD ID, TAIR protein ID, EuPathDb ID; from
 #' the [SwissPalm website](https://swisspalm.org/file_formats)).
 #' @param dataset Which dataset to use in swissPalm. Will be set to `"all"` if
 #' not a value from `swisspalm::datasets`. Default = `"all"` (all datasets).
-#' @param species Which dataset to use in swissPalm. Will be set to `"all"` if
-#' not a value from `swisspalm::species`. Default = `"all"` (all datasets).
+#' @param species Which species to use in swissPalm. Will be set to `""` if
+#' not a value from `swisspalm::species`. Default = `""` (all species).
 #' @return Data frame with palmitoylation data for proteins in `protein_id`,
-#' and notes for proteins not found in SwissPalm.
+#' and notes for why any elements in `protein_id` were not found in SwissPalm.
 #' @examples
-#' swisspalmR::swissPalm(protein_id = c("P05067", "O00161", "P04899", "P98019"))
-#' 
+#' \donttest{
+#' swisspalmR::swissPalm(query_id = c("P05067", "O00161", "P04899", "P98019"))
+#' }
+#'
 #' # Use 'species' parameter to limit your results to those for a species of 
 #' # interest
+#' \donttest{
 #' swisspalmR::swissPalm(
-#'   protein_id = c("P05067", "O00161", "P04899", "P98019"),
+#'   query_id = c("P05067", "O00161", "P04899", "P98019"),
 #'   species = swisspalmR::species["Mallard duck"]
 #'   )
+#' }
 #' @export
-swissPalm <- function(protein_id, dataset = "all", species = NULL) {
-  if (length(protein_id) == 0) {
-    cli::cli_abort("{.var protein_id} must have length >= 1")
+swissPalm <- function(query_id, dataset = "all", species = "") {
+  if (length(query_id) == 0) {
+    cli::cli_abort(message = "{.var query_id} must have length >= 1")
   }
-  species <- if (is.null(species)) "" else species
+  if (!is.character(query_id)) {
+    cli::cli_abort(message = "{.var query_id} must be a character vector")
+  }
+  # This function will reset species/dataset if not valid
   check_species_dataset(species, dataset)
 
   user <- "swisspalmR package (https://simpar1471.github.io/swisspalmR/)"
-  GET_req <- "https://swisspalm.org/proteins/refresh_form?first_load=true" |>
-    httr2::request() |>
-    httr2::req_user_agent(string = user)
+  GET_req <- swisspalm_GET_req(user)
   GET_resp <- httr2::req_perform(GET_req)
   GET_html <- httr2::resp_body_string(GET_resp) |>
     strsplit(split = "\n") |>
@@ -41,7 +46,7 @@ swissPalm <- function(protein_id, dataset = "all", species = NULL) {
   GET_resp_cookie <- swissPalm_cookie(GET_resp)
   POST_req_cookie <- GET_resp_cookie
 
-  headers <- list(
+  headers_html <- list(
     `Accept` = "*/*",
     `Accept-Encoding` = "gzip, deflate, br",
     `Accept-Language` = "en-GB,en-US;q=0.9,en;q=0.8,hy;q=0.7",
@@ -54,44 +59,66 @@ swissPalm <- function(protein_id, dataset = "all", species = NULL) {
     `X-Requested-With` = "XMLHttpRequest"
   )
 
+  # Save protein IDs to temporary file which will be uploaded to SwissPalm
   path_upload <- tempfile(pattern = "swissPalm-", fileext = ".txt")
   writeLines(con = path_upload,
-             paste(protein_id, collapse = "\n"))
+             paste(query_id, collapse = "\n"))
   on.exit(expr = try(unlink(path_upload)), add = TRUE)
 
-  xhr_li <- list(file = curl::form_file(path_upload),
-                 free_text = "",
-                 limit = "100",
-                 dataset = as.character(dataset),
-                 organism_id = as.character(species),
-                 load_times = "1",
-                 edit_cart = "",
-                 render_stats = "0",
-                 use_cart = "0",
-                 format = "html")
+  xhr_html <- list(file = curl::form_file(path_upload),
+                   free_text = "",
+                   limit = "100",
+                   dataset = as.character(dataset),
+                   organism_id = as.character(species),
+                   load_times = "1",
+                   edit_cart = "",
+                   render_stats = "0",
+                   use_cart = "0",
+                   format = "html")
 
-  POST_req <- httr2::request("https://swisspalm.org/proteins/search") |>
-    httr2::req_headers(!!!headers) |>
-    httr2::req_user_agent(string = user) |>
-    httr2::req_body_multipart(!!!xhr_li)
+  # Request HTML webpage for given proteins - necessary to determine which
+  # proteins are not found in the request
+  POST_req_html <- swisspalm_POST_req(headers_html, xhr_html, user)
 
-  POST_resp <- httr2::req_perform(POST_req)
+  POST_resp <- httr2::req_perform(POST_req_html)
   POST_html <- httr2::resp_body_string(POST_resp) |>
     rvest::read_html()
+  swissPalm_not_found <- extract_not_found(POST_html)
 
-  table <- rvest::html_table(POST_html, fill = TRUE)[[1]]
-  not_found <- extract_not_found(POST_html)
-  if (length(table) != 0) {
-    swissPalm_table <- table |>
-      as.data.frame() |>
-      dplyr::select(!(`UniProt status` | 1)) |>
-      dplyr::mutate(Found = "found", .after = `Query identifier`)
-    if (nrow(not_found) == 0) {
-      return(swissPalm_table)
-    } else {
-      return(suppressMessages(dplyr::full_join(swissPalm_table, not_found)))
-    }
+
+  # Request palmitoylation information as text (i.e. same as text download on
+  # website). This requires CSRF token to be provided in XHR rather than in
+  # headers, so swaps occur now.
+  headers_txt <- headers_html[names(headers_html) != "X-CSRF-Token"]
+  headers_txt[["Cookie"]] <- swissPalm_cookie(POST_resp)
+
+  xhr_li_txt <- xhr_html
+  xhr_li_txt[["format"]] <- "text"
+  xhr_li_txt[["authenticity_token"]] <- headers_html[["X-CSRF-Token"]]
+
+  # POST request for txt download
+  POST_req_txt <- swisspalm_POST_req(headers_txt, xhr_li_txt, user)
+  POST_resp_txt <- httr2::req_perform(POST_req_txt)
+  POST_resp_str <- httr2::resp_body_string(POST_resp_txt) |>
+    gsub(pattern = "Yes", replacement = "TRUE") |>
+    gsub(pattern = "No", replacement = "FALSE")
+
+  suppressWarnings( # Otherwise no results found will throw warning
+    swissPalm_table <- utils::read.table(
+      text = POST_resp_str, sep = "\t", header = TRUE,
+      col.names = swissPalm_table_colnames()
+    ) |>
+      convert_swissPalm_table_listcols()
+  )
+  if (nrow(swissPalm_table) >= 1) {
+    swissPalm_table$Found_in_SwissPalm <- found_as_factor(x = "found")
+  }
+
+  if (is.null(swissPalm_not_found)) {
+    return(swissPalm_table) # Every query ID was found
+  } else if (nrow(swissPalm_not_found) == length(query_id)) {
+    return(swissPalm_not_found) # Every query ID was not found
   } else {
-    return(not_found)
+    rbind(swissPalm_table, swissPalm_not_found) # Some found, some not
   }
 }
